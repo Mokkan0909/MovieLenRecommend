ae90b66c2fd908efe72a82523c93ef8ed0abe27c
Mokkan0909 <40558235+Mokkan0909@users.noreply.github.com>
2018-06-25 16:14:57 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 25 15:23:36 2018
+
+@author: Tomoharu
+"""
+
+from distutils.core import setup
+
+setup(
+    name='MovieLenRecommendation',
+    version='0.0.1',
+    description='Recommendation of movies by based user or item',
+    author='Tomoharu Mori',
+    author_email='morningshine726@gmail.com',
+    install_requires = ['math'],
+    url='https://github.com/Mokkan0909/MovieLenRecommend.git',
+    Programming_Language = "Python :: 3"
+)
